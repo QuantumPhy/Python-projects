e65f6e838cab31068c05f17bc5a8e067cb41e687
ricsinaruto <ricsinaruto@hotmail.com>
2017-05-26 10:42:52 +0200

    organized the chatbot projects

---
+++ seq2seq_chatbot_projects/practical_seq2seq_cornell/03-Twitter-chatbot.py
@@ -0,0 +1,48 @@
+
+# In[1]:
+
+import tensorflow as tf
+import numpy as np
+
+# preprocessed data
+from datasets.twitter import data
+import data_utils
+
+# load data from pickle and npy files
+metadata, idx_q, idx_a = data.load_data(PATH='datasets/cornell_corpus/')
+(trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
+
+# parameters 
+xseq_len = trainX.shape[-1]
+yseq_len = trainY.shape[-1]
+batch_size = 16
+xvocab_size = len(metadata['idx2w'])  
+yvocab_size = xvocab_size
+emb_dim = 512
+
+import seq2seq_wrapper
+import importlib
+importlib.reload(seq2seq_wrapper)
+
+# In[7]:
+
+model = seq2seq_wrapper.Seq2Seq(xseq_len=xseq_len,
+                               yseq_len=yseq_len,
+                               xvocab_size=xvocab_size,
+                               yvocab_size=yvocab_size,
+                               ckpt_path='ckpt/cornell_corpus/',
+                               emb_dim=emb_dim,
+                               num_layers=3
+                               )
+
+
+# In[8]:
+
+val_batch_gen = data_utils.rand_batch_gen(validX, validY, 128)
+test_batch_gen = data_utils.rand_batch_gen(testX, testY, 128)
+train_batch_gen = data_utils.rand_batch_gen(trainX, trainY, batch_size)
+
+
+# In[9]:
+#sess = model.restore_last_session()
+sess = model.train(train_batch_gen, val_batch_gen)
